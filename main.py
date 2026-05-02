daab756ea3dfaa8e80163dfffe71dfe7d6bbd992
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-02 04:48:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from utils import (read_video,
 
 from trackers import PlayerTracker, BallTracker
 from court_line_detector.court_line_detector import CourtLineDetector
+import cv2
 
 def main():
     # Reading video:
@@ -25,11 +26,17 @@ def main():
                                                      stub_path = "tracker_stubs/ball_detections.pk1"
                                                      )
     
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
+    
     # Court Line Detector model:
 
     court_model_path = "models/keypoints_model.pth"
     court_line_detector = CourtLineDetector(court_model_path)
     court_keypoints = court_line_detector.predict(video_frames[0])
+
+    # Choose players:
+
+    player_detections = player_tracker.choose_and_filter_players(court_keypoints, player_detections)
 
     # Draw outputs:
 
@@ -41,6 +48,11 @@ def main():
     # Drawing the court:
 
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+
+    # Draw frame number on top left corner:
+
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 
 
