2a3f402b6da94ce9aa578d12545cdaeae394f5c5
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-02 04:52:19 +0100

    Update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from utils import (read_video,
                    save_video)
 
 from trackers import PlayerTracker, BallTracker
+from court_line_detector.court_line_detector import CourtLineDetector
+import cv2
 
 def main():
     # Reading video:
@@ -20,9 +22,21 @@ def main():
                                                      )
     
     ball_detections = ball_tracker.detect_frames(video_frames, 
-                                                     read_from_stub = False,
+                                                     read_from_stub = True,
                                                      stub_path = "tracker_stubs/ball_detections.pk1"
                                                      )
+    
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
+    
+    # Court Line Detector model:
+
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
+
+    # Choose players:
+
+    player_detections = player_tracker.choose_and_filter_players(court_keypoints, player_detections)
 
     # Draw outputs:
 
@@ -30,6 +44,15 @@ def main():
 
     output_video_frames = player_tracker.draw_bbboxes(video_frames, player_detections)
     output_video_frames = ball_tracker.draw_bbboxes(output_video_frames, ball_detections)
+
+    # Drawing the court:
+
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+
+    # Draw frame number on top left corner:
+
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 
 
